6204878c541dd3622ca377be602cad2cd9fb242f
Dyar Faradj <dyar.98@hotmail.com>
2023-11-02 18:03:49 +0100

    fixes

--- dohome/run.sh
+++ dohome/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "Starteddddt1"
-echo "Finished"
+
+# Optional: Add any setup or pre-start commands here
+echo "Started DoHome Addon"
+# Start your addon's main service or script
+python3 /src/__init__.py
