6acd75cd7b2284ce667b9a7d55dcadc21e1d523b
Dyar Faradj <dyar.98@hotmail.com>
2023-11-02 18:07:51 +0100

    Update run.sh

--- dohome/run.sh
+++ dohome/run.sh
@@ -2,5 +2,5 @@
 
 # Optional: Add any setup or pre-start commands here
 echo "Started DoHome Addon"
-# Start your addon's main service or script
-python3 /src/__init__.py
+# Start your addon's main service or script as the main process
+exec python3 /src/__init__.py
